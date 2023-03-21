fc9e75b3435bee12e34982e1f1e5b6d08aefe1b7
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-21 15:13:35 -0400

    Update CompatWarningState.hx

--- source/CompatWarningState.hx
+++ source/CompatWarningState.hx
@@ -24,7 +24,7 @@ class CompatWarningState extends MusicBeatState
 
     public override function create():Void
     {
-        #if (flixel < "5.0.0") 
+        #if (flixel < "5.2.2") 
         bg = new FlxBackdrop(Paths.image('ui/checkeredBG', 'preload'), 1, 1, true, true, 1, 1);
         bg.antialiasing = true;
         bg.color = langaugeList[curLanguageSelected].langaugeColor;

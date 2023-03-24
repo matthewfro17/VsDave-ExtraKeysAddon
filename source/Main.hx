8cfcc3d3a0ba5ee17ddd64406c2d3b3e0ddd40a0
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-23 21:22:56 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = StartStateSelector; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	public static var framerate:Int = 60; // How many frames per second the game should run at.
+	public static var framerate:Int = 144; // How many frames per second the game should run at.
 	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var keyAmmo:Array<Int> = [4, 5, 6, 7, 9, 12];
@@ -29,12 +29,38 @@ class Main extends Sprite
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
+	public static function main():Void
+	{
+		Lib.current.addChild(new Main());
+	}
+
 	public function new()
 	{
 		super();
 
-		SUtil.uncaughtErrorHandler();
+		if (stage != null)
+			init();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, init);
+	}
 
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+
+	public static function toggleFuckedFPS(toggle:Bool)
+	{
+		fps.fuckFps = toggle;
+	}
+
+	private function setupGame():Void
+	{
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
@@ -48,19 +74,13 @@ class Main extends Sprite
 		}
 
 		initialState = StartStateSelector;
+                addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
-		SUtil.check();
-
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.2.2") zoom, framerate, framerate, skipSplash, startFullscreen));
-
+		#if !android
 		fps = new FpsDisplay(10, 3, 0xFFFFFF);
 		var fpsFormat = new TextFormat("Comic Sans MS Bold", 15, 0xFFFFFF, true);
 		fps.defaultTextFormat = fpsFormat;
 		addChild(fps);
-	}
-
-	public static function toggleFuckedFPS(toggle:Bool)
-	{
-		fps.fuckFps = toggle;
+		#end
 	}
 }
